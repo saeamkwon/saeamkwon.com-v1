a26f5131d0de9e72e094f4e6d58fb56d00b2a181
Mark Miyashita <negativetwelve@gmail.com>
2017-02-07 16:30:18 -0800

    Modify the build script to not use git worktree

--- bin/deploy.sh
+++ bin/deploy.sh
@@ -10,14 +10,13 @@ then
   exit 1;
 fi
 
-echo "Deleting old publication"
+echo "Deleting last build."
 rm -rf public
 mkdir public
-git worktree prune
-rm -rf .git/worktrees/public/
+cd public && git remote add origin git@github.com:minikwon/saeamkwon.com.git
 
 echo "Checking out gh-pages branch into public"
-git worktree add -B gh-pages public origin/gh-pages
+cd public && git fetch origin/gh-pages
 
 echo "Removing existing files"
 rm -rf public/*
